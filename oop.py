1ec36a9b6f706270b09a995d66750852d239631f
Daniell2019 <49656898+Daniell2019@users.noreply.github.com>
2019-05-23 17:10:30 -0300

    Update oop.py

--- oop.py
+++ oop.py
@@ -1,5 +1,32 @@
 class pessoa:
-  def __init__(self):
-    self.nome = nome
-    self.rg = rg
-    
+    def __init__(self):
+	    self.nome = nome
+	    self.rg = rg
+	    self.cpf = cpf
+	    self.anoNasc = anoNasc
+	    self.mesNasc = mesNasc
+	    self.diaNasc = diaNasc
+	    self.sexo = sexo
+    def imprime(self):
+        print("success %d"%(self.nome, self.rg))
+
+class professor:
+    def __init__(self):
+        self.formacao = formacao
+        self.nivel = nivel
+        self.disciplina = disciplina
+        
+
+class Matricula:
+    def __init__(self):
+        self.id = id
+        self.mesMatricula = mesMatricula
+        self.anoMatricula = anoMatricula
+matricula()
+
+class Curso:
+    def __init__(self):
+        self.titulo = titulo
+        self.descricao = descricao
+        self.valor = valor
+        self.sala = sala
