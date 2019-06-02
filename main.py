c8b75a5abb78a19e8ff8a0ab6d555fb31a8030ed
Daniell2019 <49656898+Daniell2019@users.noreply.github.com>
2019-06-02 12:15:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,23 @@ def menu_principal():
     while True:
         escolha = int(input("Escolha a opção desejada: "))
         if escolha == 1:
-            dados.cadastrar_pessoas()
-        if escolha == 2:
-            dados.listar_pessoas()
-        if escolha == 3:
+            dados = informaçoes_funcionario()
+            funcionario_a = funcionario(dados.get('nome'),dados.get('rg'),dados.get('cpf'),
+            dados.get('dataNascimento'),dados.get('sexo'), dados.get('matricula'),dados.get('setor'),
+            dados.get('nivel'))
+            dados.cadastrar_funcionario()
+        elif escolha == 2:
+            dados = informacoes_professor()
+            professor_a = professor(dados.get('nome'),dados.get('rg'),dados.get('cpf'),
+            dados.get('dataNascimento'),dados.get('sexo'), dados.get('matricula'),dados.get('setor'),
+            dados.get('nivel'), dados.get('formacao'), dados.get('disciplina'))
+            dados.cadastrar_professor()
+        elif escolha == 3:
+            funcionario.exibir_todos()
+            
+        elif escolha == 0:
             break
-        else:
-            print("Opção inválida!\n")
+
 
 menu_principal()
 
