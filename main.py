f07b8b002928134efa8332778f1267b164f8c8d3
Daniell2019 <49656898+Daniell2019@users.noreply.github.com>
2019-06-02 11:53:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from oop import *
 from Dados do Menu import *
 
-"""p = pessoa()
-    dará erro pois a classe pessoa não pode ser instanciada diretamente
-"""
+def menu_principal():
+    print(20 * "-=")
+    print("       Menu")
+    print(20 * "-=")
 
+    print("--------------------\n"
+          "1 - Cadastrar \n"
+          "2 - Listar \n"
+          "--------------------")
+
+    while True:
+        escolha = int(input("Escolha a opção desejada: "))
+        if escolha == 1:
+            dados.cadastrar_pessoas()
+        if escolha == 2:
+            dados.listar_pessoas()
+        if escolha == 3:
+            break
+        else:
+            print("Opção inválida!\n")
+
+menu_principal()
 
 
 
