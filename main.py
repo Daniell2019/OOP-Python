54c29484ba8e6fec83e3c5bb0168d234418179a4
Daniell2019 <49656898+Daniell2019@users.noreply.github.com>
2019-06-02 13:09:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,6 @@ from oop import *
 from Dados do Menu import *
 
 def menu_principal():
-    print(20 * "-=")
-    print("       Menu")
-    print(20 * "-=")
-
-    print("--------------------\n"
-          "1 - Cadastrar \n"
-          "2 - Listar \n"
-          "--------------------")
 
     while True:
         escolha = int(input("Escolha a opção desejada: "))
@@ -31,8 +23,18 @@ def menu_principal():
         elif escolha == 0:
             break
 
+def mostrar_indice():
+    print(20 * "-=")
+    print("       Menu")
+    print(20 * "-=")
 
-menu_principal()
+    print("--------------------\n"
+          "1 - Cadastrar \n"
+          "2 - Listar \n"
+          "--------------------")
+
+    if __name__ == '__main__':
+        menu_principal()
 
 
 
